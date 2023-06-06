c26000be968f16b0e78b313dd1cbdafcfb50f73d
jimmygranadino <jimmy.granadino@gmail.com>
2023-06-06 15:41:23 -0700

    storyboard modal for rules+outlet collection

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -11,20 +11,23 @@ class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
     
-    @IBOutlet var stopButton: UIButton!
-    @IBOutlet var restartButton: UIButton!
-    @IBOutlet var rulesButton: UIButton!
+    @IBOutlet var buttons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stopButton.layer.cornerRadius = 8
-        restartButton.layer.cornerRadius = 8
-        rulesButton.layer.cornerRadius = 8
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
         
     }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
     }
     
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
 }
